43fb4e9e85663e4473cc71c4c206c3aa17b694dc
rafael872 <rafael_alvarez_123@hotmail.com>
2016-11-17 05:08:05 -0400

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var config = {
+    "USER"    : "root",
+    "PASS"    : "rafael872",
+    "HOST"    : "ds155737.mlab.com:55737/blog",
+    "PORT"    : "27017",
+    "DATABASE" : "blog2"
+};
+var dbPath  = "mongodb://rafael872:rafael872@ds155737.mlab.com:55737/blog2"
+mongoose.connect(dbPath);
+
+var posibles_valores = ["M", "F"];
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Coloca un correo"];
+var password_validation = {
+    validator: function (p) {
+        return this.password_confirmation == p;
+    },
+    message: "la contraseña no es igual"
+};
+
+
+var user_schema = new Schema({
+    name: String,
+    username: {type: String, required: true, maxlength: [50, "username Muy grande"]},
+    password: {type: String, minlength: [8, "Password muy corto"], validate: password_validation},
+    age: {
+        type: Number,
+        min: [5, "La edad no puede ser menor que 5"],
+        max: [100, "La edad no puede ser mayor que 100"]
+    },
+    email: {type: String, required: "Debe ingresar un correo", match: email_match},
+    date_of_birth: Date,
+    sex: {type: String, enum: {values: posibles_valores, message: "Opción no válida"}}
+});
+
+user_schema.virtual("password_confirmation").get(function () {
+    return this.p_c;
+}).set(function (password) {
+    this.p_c = password;
+});
+
+
+/* user_schema.virtual("full_name").get(function(){
+ return this.name + this.last_name;
+ }).set(function(full_name){
+ var words = full_name.split(" ");
+ this.name= words[0];
+ this.last_name = words[1];
+ }) */
+
+var User = mongoose.model("User", user_schema);
+
+module.exports.User = User;
